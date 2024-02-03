0d771635d8a38ae6324fa7ab6df54da4f59a5cae
Ryan Delos Santos <delossantosrryan@gmail.com>
2024-02-02 18:31:49 -0800

    headings

--- src/App.js
+++ src/App.js
@@ -168,6 +168,14 @@ const InputWithLabel = ({
 
 const List = ({ list, onRemoveItem }) => (
   <ul>
+    <li style={{ display: 'flex' }}>
+      <span style={{ width: '40%' }}>Title</span>
+      <span style={{ width: '30%' }}>Author</span>
+      <span style={{ width: '10%' }}>Comments</span>
+      <span style={{ width: '10%' }}>Points</span>
+      <span style={{ width: '10%' }}>Actions</span>
+    </li>
+
     {list.map((item) => (
       <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />
     ))}
